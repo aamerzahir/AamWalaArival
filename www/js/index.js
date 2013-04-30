20b95140b85218868b607174a1ddb3729dab8f7e
Aamer Zahir <aamer.zahir@gmail.com>
2013-05-01 02:35:54 +0530

    update with UI

--- www/js/index.js
+++ www/js/index.js
@@ -38,11 +38,9 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
+        parentElement.setAttribute('style', 'display:none;');
 
         console.log('Received Event: ' + id);
     }
